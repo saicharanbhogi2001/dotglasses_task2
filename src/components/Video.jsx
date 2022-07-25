ef93ab6fffa0aada1782a690feb2f9066e7ba4b1
saicharanbhogi2001 <84128500+saicharanbhogi2001@users.noreply.github.com>
2022-07-25 21:01:18 +0530

    Update Video.jsx

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Video.css';
 import {AiFillPlayCircle} from 'react-icons/ai'
-import Spects from 'C:/Users/DELL/dotglasses/dotglasses/src/images/spects1.png'
+import Spects from 'C:/Users/ASUS/Desktop/dotglasses/dotglasses/src/images/spects1.png'
 function Video() {
   return (
     <div className='video'>
